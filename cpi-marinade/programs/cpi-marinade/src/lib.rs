c502915fce1dcf8d3c7f782bd1f8d2fa2896eaee
Ondra Chaloupka <chalda@chainkeepers.io>
2022-10-11 16:17:00 +0200

    cpi marinade to just work

--- cpi-marinade/programs/cpi-marinade/src/lib.rs
+++ cpi-marinade/programs/cpi-marinade/src/lib.rs
@@ -1,15 +1,73 @@
 use anchor_lang::prelude::*;
+use marinade_cpi::{
+    program::MarinadeFinance,
+    cpi::accounts::Deposit,
+    cpi::deposit,
+};
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("Em26cSRXE9fxSPNnkHUh8Wmvqh279Jo7StWuCDTCLLSe");
 
 #[program]
 pub mod cpi_marinade {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
+    pub fn call_deposit(ctx: Context<CallDeposit>, lamports: u64) -> Result<()> {
+        deposit(ctx.accounts.set_data_ctx(), lamports)
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct CallDeposit<'info> {
+    pub marinade_program: Program<'info, MarinadeFinance>,
+
+    // to pass all these data further
+    /// CHECK:
+    #[account(mut)]
+    pub state: AccountInfo<'info>, // marinade state
+    /// CHECK:
+    #[account(mut)]
+    pub msol_mint: AccountInfo<'info>,
+    /// CHECK:
+    #[account(mut)]
+    pub liq_pool_sol_leg_pda: AccountInfo<'info>,
+    /// CHECK:
+    #[account(mut)]
+    pub liq_pool_msol_leg: AccountInfo<'info>,
+    /// CHECK:
+    pub liq_pool_msol_leg_authority: AccountInfo<'info>,
+    /// CHECK:
+    #[account(mut)]
+    pub reserve_pda: AccountInfo<'info>,
+    /// CHECK:
+    #[account(mut, signer)]
+    pub transfer_from: AccountInfo<'info>,
+    /// CHECK:
+    #[account(mut)]
+    pub mint_to: AccountInfo<'info>,
+    /// CHECK:
+    pub msol_mint_authority: AccountInfo<'info>,
+    /// CHECK:
+    pub system_program: AccountInfo<'info>,
+    /// CHECK:
+    pub token_program: AccountInfo<'info>,
+}
+
+impl<'info> CallDeposit<'info> {
+    pub fn set_data_ctx(&self) -> CpiContext<'_, '_, '_, 'info, Deposit<'info>> {
+        let cpi_program = self.marinade_program.to_account_info();
+        let cpi_accounts = Deposit {
+            state: self.state.clone(),
+            msol_mint: self.msol_mint.clone(),
+            liq_pool_sol_leg_pda: self.liq_pool_sol_leg_pda.clone(),
+            liq_pool_msol_leg: self.liq_pool_msol_leg.clone(),
+            liq_pool_msol_leg_authority: self.liq_pool_msol_leg_authority.clone(),
+            reserve_pda: self.reserve_pda.clone(),
+            transfer_from: self.transfer_from.clone(),
+            mint_to: self.mint_to.clone(),
+            msol_mint_authority: self.msol_mint_authority.clone(),
+            system_program: self.system_program.clone(),
+            token_program: self.token_program.clone(),
+        };
+        CpiContext::new(cpi_program, cpi_accounts)
+    }
+}
--- cpi-marinade/tests/cpi-marinade.ts
+++ cpi-marinade/tests/cpi-marinade.ts
@@ -1,6 +1,26 @@
 import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
+import { Program, web3, BN, Wallet} from "@project-serum/anchor";
+import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
+import { ConfirmOptions } from "@solana/web3.js";
 import { CpiMarinade } from "../target/types/cpi_marinade";
+
+const SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID = new web3.PublicKey(
+  'ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL',
+);
+
+async function findAssociatedTokenAddress(
+    walletAddress: web3.PublicKey,
+    tokenMintAddress: web3.PublicKey
+): Promise<web3.PublicKey> {
+    return (await web3.PublicKey.findProgramAddress(
+        [
+            walletAddress.toBuffer(),
+            TOKEN_PROGRAM_ID.toBuffer(),
+            tokenMintAddress.toBuffer(),
+        ],
+        SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID
+    ))[0];
+}
 
 describe("cpi-marinade", () => {
   // Configure the client to use the local cluster.
@@ -8,9 +28,42 @@ describe("cpi-marinade", () => {
 
   const program = anchor.workspace.CpiMarinade as Program<CpiMarinade>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+  // ./target/debug/marinade -i `cat /home/chalda/marinade/marinade-anchor/keys/instance.pubkey` show
+  // Instance : Pubkey(5kZPmcZHranmRWb3usuKugN9xeLQciZAotc1vrxvjshu)
+  const state = new web3.PublicKey("5kZPmcZHranmRWb3usuKugN9xeLQciZAotc1vrxvjshu");
+  // reserve 0.00203928 SOL (PDA) 58RZTF6YBqVFq35YYdK3s33385Er4LXeWDehRf6SvXGH
+  const reservePda = new web3.PublicKey("58RZTF6YBqVFq35YYdK3s33385Er4LXeWDehRf6SvXGH");
+  // mSOL supply 0 mint HKbs3nrjM4A7K6k3BBHhir1QwCpDtEYQmgarqe9bhd95 auth 4v8a6DKL4eQU72dFxArtXd3BAQGw7nLmvNDbkr6beq4P
+  const msolMint = new web3.PublicKey("HKbs3nrjM4A7K6k3BBHhir1QwCpDtEYQmgarqe9bhd95");
+  const msolMintAuthority = new web3.PublicKey("4v8a6DKL4eQU72dFxArtXd3BAQGw7nLmvNDbkr6beq4P");
+  // -- Liq-Pool ---------------
+  // mSOL-SOL-LP supply 0 mint  ...
+  // mSOL  0.000000000 account F6CT9vkDwrCLVtmaAcqug47sq1N7Cf5d2E5Zhx1LTTxD auth 542TYqMNii7zRcFrEpq4p76cqDN6CsyqMd6nFWvU7HYg
+  // SOL   0.00203928 account D3EcJk6c4VC7JG73SUqARKw6eZFpHdEiG3iP5T2N8YHy
+  const liqPoolMsolLeg = new web3.PublicKey("F6CT9vkDwrCLVtmaAcqug47sq1N7Cf5d2E5Zhx1LTTxD");
+  const liqPoolMsolLegAuthority = new web3.PublicKey("542TYqMNii7zRcFrEpq4p76cqDN6CsyqMd6nFWvU7HYg");
+  const liqPoolSolLegPda = new web3.PublicKey("D3EcJk6c4VC7JG73SUqARKw6eZFpHdEiG3iP5T2N8YHy");
+
+  it("Calling Marinade deposit", async () => {
+    const mintTo = await findAssociatedTokenAddress(Wallet.local().publicKey, msolMint);
+    console.log("ata for local wallet", mintTo.toBase58());
+
+    const lamports = web3.LAMPORTS_PER_SOL;
+    const tx = await program.methods.callDeposit(new BN(lamports)).accounts(
+    {
+        marinadeProgram: new web3.PublicKey("MarBmsSgKXdrN1egZf5sqe1TMai9K1rChYNDJgjq7aD"),
+        state: state,
+        msolMint: msolMint,
+        liqPoolSolLegPda: liqPoolSolLegPda,
+        liqPoolMsolLeg: liqPoolMsolLeg,
+        liqPoolMsolLegAuthority: liqPoolMsolLegAuthority,
+        reservePda: reservePda,
+        transferFrom: Wallet.local().publicKey,
+        mintTo: mintTo,
+        msolMintAuthority: msolMintAuthority,
+        systemProgram: web3.SystemProgram.programId,
+        tokenProgram: TOKEN_PROGRAM_ID,
+      }).rpc({skipPreflight: true});
     console.log("Your transaction signature", tx);
   });
 });
