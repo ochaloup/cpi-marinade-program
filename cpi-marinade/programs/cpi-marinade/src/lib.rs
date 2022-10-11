579d83123dd20923b8b376b730947dda4561ed96
Ondra Chaloupka <chalda@chainkeepers.io>
2022-10-11 10:34:54 +0200

    anchor deploy + changing program id based on the deployed one

--- cpi-marinade/programs/cpi-marinade/src/lib.rs
+++ cpi-marinade/programs/cpi-marinade/src/lib.rs
@@ -1,9 +1,10 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("GjVoL49VRjsSXjdG3FAfkXgdwk7VfAPpV9K23gx8C6Na");
 
 #[program]
 pub mod cpi_marinade {
+    use anchor_lang::solana_program::entrypoint::ProgramResult;
     use super::*;
     pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
         Ok(())
