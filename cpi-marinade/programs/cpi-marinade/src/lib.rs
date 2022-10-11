e1b279d3214b0935efba75638336d1b578184b7f
Ondra Chaloupka <chalda@chainkeepers.io>
2022-10-11 10:43:17 +0200

    reinitialize program with cargo 0.25.0

--- cpi-marinade/programs/cpi-marinade/src/lib.rs
+++ cpi-marinade/programs/cpi-marinade/src/lib.rs
@@ -1,12 +1,12 @@
 use anchor_lang::prelude::*;
 
-declare_id!("GjVoL49VRjsSXjdG3FAfkXgdwk7VfAPpV9K23gx8C6Na");
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod cpi_marinade {
-    use anchor_lang::solana_program::entrypoint::ProgramResult;
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
 }
